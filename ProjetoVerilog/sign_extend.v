b5a51e38e7f4ac19aac1e7256f1b4c02d09bc76f
Ivan Neves Silveira Bandeira de Souza <insbs@WINDOWS.CIN.UFPE.BR>
2019-05-16 11:51:01 -0300

    Corrigindo warnings e sign_extend.v

--- ProjetoVerilog/sign_extend.v
+++ ProjetoVerilog/sign_extend.v
@@ -4,6 +4,6 @@ module sign_extend(input wire [15:0] wireIn, input wire sign, output wire [31:0]
 	wire [31:0] Data_out;
 	
 	assign withSign  = (wireIn[15]) ? {{16{1'b1}},wireIn} : {{16{1'b0}},wireIn};// com sinal  checa se o maior bit é um ou zero, se for um preenche com 1, se for zero preenche com zero
-	assign Data_out = (sign) ? withSign : {{16{1'b0}},wireIn};// sign checa se a entrada é com ou sem sinal, se for com, a resposta é withSign, se não apenas completa com zeros
+	assign wireOut = (sign) ? withSign : {{16{1'b0}},wireIn};// sign checa se a entrada é com ou sem sinal, se for com, a resposta é withSign, se não apenas completa com zeros
 		
 endmodule
